59004b6f2db5c9af90a7799afb112b69534c98d6
Kenkeluke <halimahogunbiyi@gmail.com>
2022-07-11 22:24:03 +0000

    handling strings and characters

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+  #define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int print_char(va_list ap);
+int print_string(va_list ap);
+
+
+#endif /* MAIN_H */
---
+++ str_char_percent.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * print_string - Function that prints a string
+ * Description: This function prints
+ * 
+ * @ap: list of variable arguments
+ * return: number of character ina string 
+ * 
+ */
+
+int print_string(va_list ap)
+{
+int i;
+char *point;
+point = (va_arg(ap, char*));
+
+if(point == 0){
+_putchar('\n');
+}
+
+for( i = 0; point[i] != '\0'; i++)
+{
+_putchar(point[i]);
+return(i);
+}
+}
+
+/**
+ * print_char - Function that prints a character
+ * Description: This function prints
+ * 
+ * @ap: varaiable parameter
+ * return: number of character 
+ * 
+ */
+
+int print_char(va_list ap)
+{
+
+char c;
+c = (va_arg(ap, int));
+
+_putchar(c);
+return(1);
+
+}
+
+
+/**
+ * print_percent - Function that prints a character
+ * Description: This function prints
+ * 
+ * @ap: varaiable parameter
+ * return: number of character 
+ * 
+ */
+
+int print_char(va_list ap)
+{
+
+char c;
+c = (va_arg(ap, int));
+
+_putchar(c);
+return(1);
+
+}
