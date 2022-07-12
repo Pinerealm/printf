f1cd7ce76eb6811343bdd8616e02d097c851dcdc
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-12 12:57:37 -0700

    Refactored _printf code

--- _printf.c
+++ _printf.c
@@ -1,6 +1,43 @@
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "main.h"
+
+/**
+ * _printf - custom-made printf implementation
+ *
+ * @format: format specifier char
+ * @...: optional arguments
+ *
+ * Return: number of bytes written
+ */
 
 int _printf(const char *format, ...)
 {
+	unsigned int b_cnt = 0, i = 0;
+	va_list ap;
 
+	if (!format || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+
+			b_cnt += char_num_handler(format[i], ap);
+
+		}
+		else
+		{
+			_putchar(format[i]);
+			b_cnt++;
+		}
+		i++;
+	}
+	va_end(ap);
+	return (b_cnt);
+}
