c4c016528ee18b206b3c504643c626070f4758ae
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-12 12:58:46 -0700

    Support functions for _printf

---
+++ print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_number - Prints a number
+* @n: The number to print
+*
+* Return: no of bytes printed
+*/
+
+int print_number(int n)
+{
+	unsigned int num = n, cnt = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		cnt++;
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+		cnt++;
+	}
+	_putchar(num % 10 + '0');
+	cnt++;
+	
+
+	return (cnt);
+}
