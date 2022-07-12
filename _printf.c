6e976f0b13c9e9eed2de8a36d610087c754a61f6
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-12 00:17:19 -0700

    C function: _printf

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - custom-made printf implementation
+ *
+ * @format: format specifier char
+ * @...: optional arguments
+ *
+ * Return: number of bytes written
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	unsigned int b_cnt = 0, i = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			b_cnt += char_num_handler(format[i], ap);
+		}
+		else
+		{
+			_putchar(format[i]);
+			b_cnt++;
+		}
+		i++;
+	}
+	va_end(ap);
+	return (b_cnt);
+}
