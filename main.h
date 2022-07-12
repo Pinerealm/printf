af35fce4e37932fd8c8cc2836bc6be1da2bdb83b
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-12 00:29:13 -0700

    Added prototypes

--- main.h
+++ main.h
@@ -1,12 +1,14 @@
 #ifndef MAIN_H
-  #define MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
 
+int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list ap);
 int print_string(va_list ap);
-
+int print_number(int n);
+int char_num_handler(const char c, va_list ap);
 
 #endif /* MAIN_H */
