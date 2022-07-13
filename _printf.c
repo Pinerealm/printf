11f5bb46e69752eebbe7c9ab47898c645a99f23c
kenkeluke <halimahogunbiyi@gmail.com>
2022-07-12 20:34:44 -0700

    fixing betty specifications

--- _printf.c
+++ _printf.c
@@ -7,42 +7,55 @@
  */
 int _printf(const char *format, ...)
 {
-  va_list vl; int i = 0, j = 0; char buff[100] = {0}, tmp[20]; char *str_arg;
-  va_start(vl, format);
-  while (format && format[i])
-  {
-    if (format[i] == '%')
-    {
-      i++;
-      switch (format[i])
-      {
-      case '%':
-      {buff[j] = '%'; j++; break;}
-      case 'c':
-      {buff[j] = (char)va_arg(vl, int);j++; break; }
-      case 'd':
-      case 'i':
-      {
-        _itoa(va_arg(vl, int), tmp, 10);
-        strcpy(&buff[j], tmp);
-        j += strlen(tmp); break;
-      }
-      case 's':
-      {
-        str_arg = va_arg(vl, char *);
-        strcpy(&buff[j], str_arg);
-        j += strlen(str_arg);
-        break;
-      }
-      }
-    }
-    else
-    {
-      buff[j] = format[i];
-      j++;
-    }
-    i++;
-  }
-  fwrite(buff, j, 1, stdout); va_end(vl);
+	va_list vl;
+	int i = 0, j = 0;
+	char buff[100] = {0}, tmp[20];
+	char *str_arg;
+	va_start(vl, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+			case '%':
+			{
+				buff[j] = '%';
+				j++;
+				break;
+			}
+			case 'c':
+			{
+				buff[j] = (char)va_arg(vl, int);
+				j++;
+				break;
+			}
+			case 'd':
+			case 'i':
+			{
+				_itoa(va_arg(vl, int), tmp, 10);
+				strcpy(&buff[j], tmp);
+				j += strlen(tmp);
+				break;
+			}
+			case 's':
+			{
+				str_arg = va_arg(vl, char *);
+				strcpy(&buff[j], str_arg);
+				j += strlen(str_arg);
+				break;
+			}
+			}
+		}
+		else
+		{
+			buff[j] = format[i];
+			j++;
+		}
+		i++;
+	}
+  fwrite(buff, j, 1, stdout);
+  va_end(vl);
   return (j);
 }
