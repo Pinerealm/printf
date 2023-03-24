f95ed2355fc731bac0cbff05dc0c118191561883
Pinerealm <osalami09@gmail.com>
2023-03-24 22:34:24 +0300

    Re-implement format specifiers for c,s,d,i and b

--- _printf.c
+++ _printf.c
@@ -1,50 +1,132 @@
 #include "main.h"
 
 /**
- * _printf - print formatted output to stdout
- * @format: format specifier string
+ * _printf - custom-made printf implementation
+ * @format: format string
  *
  * Return: number of bytes written
  */
 
 int _printf(const char *format, ...)
 {
-	int fmt_bytes = 0, total_bytes = 0, buff_idx = 0;
-	int i, flag, width, precision, length_mod;
 	va_list ap;
-	char buffer[BUFFER_SIZE];
+	int f_idx = 0, printed = 0;
 
 	if (!format)
 		return (-1);
-
 	va_start(ap, format);
 
-	for (i = 0; format && format[i]; i++)
+	while (format[f_idx])
 	{
-		if (format[i] != '%')
-		{
-			buffer[buff_idx++] = format[i];
-			if (buff_idx == BUFFER_SIZE)
-				print_buf(buffer, &buff_idx);
-			total_bytes++;
-		}
+		if (format[f_idx] != '%')
+			printed += _putchar(format[f_idx++]);
 		else
 		{
-			print_buf(buffer, &buff_idx);
-			flag = get_flag(format, &i);
-			width = get_width(format, &i, ap);
-			precision = get_precision(format, &i, ap);
-			length_mod = get_length_mod(format, &i);
-			i++;
-			fmt_bytes = format_printer(format, &i, ap, buffer,
-					flag, width, precision, length_mod);
-			if (fmt_bytes == -1)
-				return (-1);
-			total_bytes += fmt_bytes;
+			printed += handle_format(format[++f_idx], ap);
+			if (format[f_idx])
+				f_idx++;
 		}
 	}
-	print_buf(buffer, &buff_idx);
 	va_end(ap);
+	return (printed);
+}
 
-	return (total_bytes);
+/**
+ * handle_format - handles format specifier
+ * @c: format specifier
+ * @ap: va_list
+ *
+ * Return: number of bytes written
+ */
+int handle_format(const char c, va_list ap)
+{
+	int printed = 0;
+
+	switch (c)
+	{
+		case 'c':
+			printed += _putchar(va_arg(ap, int));
+			break;
+		case 's':
+			printed += print_string(va_arg(ap, char *));
+			break;
+		case '%':
+			printed += _putchar('%');
+			break;
+		case 'd':
+		case 'i':
+			printed += print_number(va_arg(ap, int));
+			break;
+		case 'b':
+			printed += print_binary(va_arg(ap, unsigned long int));
+			break;
+		case '\0':
+			break;
+		default:
+			printed += _putchar('%');
+			printed += _putchar(c);
+			break;
+	}
+	return (printed);
+}
+
+/**
+ * print_string - prints a string
+ * @str: string to print
+ *
+ * Return: number of bytes written
+ */
+int print_string(char *str)
+{
+	int printed = 0, idx = 0;
+	char *buf = str;
+
+	if (!buf)
+		buf = "(null)";
+	while (buf[idx])
+		printed += _putchar(buf[idx++]);
+	return (printed);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ *
+ * Return: number of bytes written
+ */
+int print_number(int n)
+{
+	int printed = 0;
+	long int num = (long int)n;
+
+	if (n < 0)
+	{
+		printed += _putchar('-');
+		num *= -1;
+	}
+	if (num / 10)
+		print_number(num / 10);
+	printed += _putchar(num % 10 + '0');
+
+	return (printed);
+}
+
+/**
+ * print_binary - prints a number in binary
+ * @n: number to print
+ *
+ * Return: number of bytes written
+ */
+int print_binary(unsigned int n)
+{
+	int printed = 0;
+	unsigned long int num = (unsigned long int)n;
+
+	if (num / 2)
+		print_binary(num / 2);
+	if (num == 0)
+		printed += _putchar('0');
+	printed += _putchar(num % 2 + '0');
+
+	return (printed);
 }
--- main.h
+++ main.h
@@ -1,71 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 
-
-#define IGNORE(x) (void)(x)
-#define BUFFER_SIZE 1024
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* LENGTH MODIFIERS */
-#define LONG_SPEC 2
-#define SHORT_SPEC 1
-
-
-/**
- * struct fmt_spec - struct for format specifier
- * @spec: format character
- * @func: function pointer that prints in specified format
- *
- * fmt_spec - typedef for struct fmt_spec
- */
-typedef struct fmt_spec
-{
-	char spec;
-	int (*func)(va_list, char *, int, int, int, int);
-} fmt_spec;
-
-
-/* FUNCTION DECLARATIONS */
 int _printf(const char *format, ...);
-void print_buf(char *buffer, int *buff_idx);
-int format_printer(const char *, int *, va_list, char *, int, int, int, int);
+int _putchar(char c);
+int handle_format(const char c, va_list ap);
+int print_string(char *str);
 
-/* Fxns to get flag, width, precision, length_mod */
-int get_flag(const char *format, int *i);
-int get_width(const char *, int *, va_list);
-int get_precision(const char *, int *, va_list);
-int get_length_mod(const char *, int *);
-
-/* Format printer functions */
-int print_char(va_list ap, char *buffer,
-	int flags, int width, int precision, int length_mod);
-int print_str(va_list, char *, int, int, int, int);
-int print_percent(va_list, char *, int, int, int, int);
-int print_int(va_list, char *, int, int, int, int);
-int print_unsigned(va_list, char *, int, int, int, int);
-
-/* Format helper functions */
-int handle_char(char c, char *buffer,
-	int flag, int width, int precision, int length_mod);
-int handle_number(int is_neg, int idx, char *buffer,
-	int flag, int width, int precision, int length_mod);
-int write_number(int idx, char *buffer, int flag, int width, int precision,
-		int len, char pad, char extra_ch);
-int handle_unsgnd(int is_neg, int idx, char *buffer,
-	int flag, int width, int precision, int length_mod);
-
-/* Support functions */
-int is_digit(char);
-long int convert_size_mode(long int num, int length_mod);
-long int convert_size_unsgnd(unsigned long int num, int length_mod);
+int print_number(int n);
+int print_binary(unsigned int n);
 
 #endif /* MAIN_H */
