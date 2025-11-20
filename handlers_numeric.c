9b8a84f990e7ede58840b94f5f428d9ce3031d2f
Pinerealm <osalami09@gmail.com>
2025-11-20 21:14:36 +0300

    Refactor numeric handlers to include flags_t parameter for improved flag handling

--- handlers_numeric.c
+++ handlers_numeric.c
@@ -3,69 +3,95 @@
 /**
  * handle_binary - writes an unsigned int in binary form
  * @args: variadic list
+ * @flags: active flags
  * @count: pointer to printed characters count
  *
  * Return: 0 on success, -1 on failure
  */
-int handle_binary(va_list args, int *count)
+int handle_binary(va_list args, flags_t *flags, int *count)
 {
 	unsigned int num = va_arg(args, unsigned int);
 
+	(void)flags;
 	return (write_unsigned_base((unsigned long int)num, 2, 0, count));
 }
 
 /**
  * handle_unsigned - writes an unsigned decimal integer
  * @args: variadic list
+ * @flags: active flags
  * @count: pointer to printed characters count
  *
  * Return: 0 on success, -1 on failure
  */
-int handle_unsigned(va_list args, int *count)
+int handle_unsigned(va_list args, flags_t *flags, int *count)
 {
 	unsigned int num = va_arg(args, unsigned int);
 
+	(void)flags;
 	return (write_unsigned_base((unsigned long int)num, 10, 0, count));
 }
 
 /**
  * handle_octal - writes an unsigned integer in octal form
  * @args: variadic list
+ * @flags: active flags
  * @count: pointer to printed characters count
  *
  * Return: 0 on success, -1 on failure
  */
-int handle_octal(va_list args, int *count)
+int handle_octal(va_list args, flags_t *flags, int *count)
 {
 	unsigned int num = va_arg(args, unsigned int);
 
+	if (flags->hash && num != 0)
+	{
+		if (write_char('0', count) == -1)
+			return (-1);
+	}
 	return (write_unsigned_base((unsigned long int)num, 8, 0, count));
 }
 
 /**
  * handle_hex_lower - writes an unsigned integer in lowercase hex
  * @args: variadic list
+ * @flags: active flags
  * @count: pointer to printed characters count
  *
  * Return: 0 on success, -1 on failure
  */
-int handle_hex_lower(va_list args, int *count)
+int handle_hex_lower(va_list args, flags_t *flags, int *count)
 {
 	unsigned int num = va_arg(args, unsigned int);
 
+	if (flags->hash && num != 0)
+	{
+		if (write_char('0', count) == -1)
+			return (-1);
+		if (write_char('x', count) == -1)
+			return (-1);
+	}
 	return (write_unsigned_base((unsigned long int)num, 16, 0, count));
 }
 
 /**
  * handle_hex_upper - writes an unsigned integer in uppercase hex
  * @args: variadic list
+ * @flags: active flags
  * @count: pointer to printed characters count
  *
  * Return: 0 on success, -1 on failure
  */
-int handle_hex_upper(va_list args, int *count)
+int handle_hex_upper(va_list args, flags_t *flags, int *count)
 {
 	unsigned int num = va_arg(args, unsigned int);
 
+	if (flags->hash && num != 0)
+	{
+		if (write_char('0', count) == -1)
+			return (-1);
+		if (write_char('X', count) == -1)
+			return (-1);
+	}
 	return (write_unsigned_base((unsigned long int)num, 16, 1, count));
 }
