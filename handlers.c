e2b738ddbea65aa1cc0e747278d50d74b59fd0f0
Pinerealm <osalami09@gmail.com>
2025-11-20 16:07:18 +0300

    Move handle_pointer function to a separate file

--- handlers.c
+++ handlers.c
@@ -100,30 +100,3 @@ int handle_string_special(va_list args, int *count)
 	}
 	return (0);
 }
-
-/**
- * handle_pointer - writes a pointer address in hexadecimal format
- * @args: variadic list
- * @count: pointer to printed characters count
- *
- * Return: 0 on success, -1 on failure
- */
-int handle_pointer(va_list args, int *count)
-{
-	void *ptr = va_arg(args, void *);
-	unsigned long int addr;
-
-	if (!ptr)
-		return (write_string("(nil)", count));
-
-	addr = (unsigned long int)ptr;
-
-	/* Print "0x" prefix */
-	if (write_char('0', count) == -1)
-		return (-1);
-	if (write_char('x', count) == -1)
-		return (-1);
-
-	/* Print address in lowercase hexadecimal */
-	return (write_unsigned_base(addr, 16, 0, count));
-}
